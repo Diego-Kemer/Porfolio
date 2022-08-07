4b17f237550e45fa16713f81fda513586263afc1
Diego <diegokemer26@gmail.com>
2022-08-06 21:13:19 -0300

    mejorando estetica

--- src/app/components/item-skill/item-skill.component.ts
+++ src/app/components/item-skill/item-skill.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-item-skill',
   templateUrl: './item-skill.component.html',
   styleUrls: ['./item-skill.component.css']
 })
-export class ItemSkillComponent implements OnInit {
+export class ItemSkillComponent implements OnInit, AfterViewInit {
   @Input() color!: string;
   @Input() num!: string;
   @Input() texto!: string;
+  @ViewChild('skill') mySkill!: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.mySkill.nativeElement.scrollTop)
   }
 
 }
